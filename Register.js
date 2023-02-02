77a3e2fbd6c411b7633b317f264b5cb64040f94b
Marco Gonzalez <marco.gonzalez@grupodigital.co>
2023-02-02 11:45:19 -0500

    add action form

--- Register.js
+++ Register.js
@@ -1,14 +1,41 @@
 const form = document.getElementById('form');
-form.addEventListener('submit', faceRegister());
-  const buttonReg = document.getElementById('buttonRegister');
-  let href = document.location.href;
-   
-const config = {
-    "urlFacialRegister": "http://localhost:90/v1/faceRegister/",
-    "urlFacialLogin": "http://localhost:90/v1/auth/login"
+const buttonReg = document.getElementById('buttonRegister');
+let href = document.location.href;
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  console.log("HOLA ENTRE");
+  console.log(e.target.dType.value);
+  let token = await login();
+  let body = {
+    "firstName": e.target.fname.value,
+    "lastName": e.target.lname.value,
+    "email": e.target.eaddress.value,
+    "documentType": e.target.dType.value,
+    "documentNumber": e.target.dNumber.value,
+    "phone": e.target.phone.value,
+    "sendToEmail": false
+  }
+  let Headers = {
+    "Authorization": "Token " + token
   }
 
-async function faceRegister(event) {
+  await axios.post(config.urlFacialRegister, body, { headers: Headers }).then(function (response) {
+    localStorage.setItem("companyReq", response.data.result.a)
+    window.location = "/PhotoRegister.html"
+  }).catch(err => {
+    console.log("REGISTRO FALLIDO ", err.response.data);
+  })
+});
+
+const config = {
+  "urlFacialRegister": "http://localhost:90/v1/faceRegister/",
+  "urlFacialLogin": "http://localhost:90/v1/auth/login"
+}
+
+async function onSubmit(event) {
+  console.log("HOLA ENTRE");
+  localStorage.setItem("entre", "entrado")
   let token = await login();
   let body = {
     "firstName": document.getElementById(fname),
@@ -18,15 +45,15 @@ async function faceRegister(event) {
     "documentNumber": document.getElementById(dNumber),
     "phone": document.getElementById(phone),
     "sendToEmail": false
-}
-console.log(document.getElementById(phone));
+  }
+  console.log(document.getElementById(phone));
   let Headers = {
     "Authorization": "Token " + token
   }
 
-  await axios.post(config.urlFacialRegister, body, {headers: Headers}).then(function (response) {
-      localStorage.setItem("companyReq", response.data.result.a)
-     
+  await axios.post(config.urlFacialRegister, body, { headers: Headers }).then(function (response) {
+    localStorage.setItem("companyReq", response.data.result.a)
+
   }).catch(err => {
     console.log("REGISTRO FALLIDO ", err.response.data);
   })
@@ -34,12 +61,12 @@ console.log(document.getElementById(phone));
 }
 
 async function login() {
-    let body = {
-      "email": "aulappprueba2@gmail.com",
-        "password": "Pruebas123@"
-    }
-  
-    return await axios.post(config.urlFacialLogin, body).then(function (response) {
-        return response.data.result.token
-    })
+  let body = {
+    "email": "aulappprueba2@gmail.com",
+    "password": "Pruebas123@"
   }
+
+  return await axios.post(config.urlFacialLogin, body).then(function (response) {
+    return response.data.result.token
+  })
+}
